009a3585bb9947cf252a316472a4ba461427a803
Florian de Savigny <floriandesavigny@Air-de-Florian.lan>
2026-02-09 08:41:21 +0100

    Fix scroll detection and redesign homepage cards

--- static/js/main.js
+++ static/js/main.js
@@ -31,11 +31,16 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     // Scroll Effect for Header
     const header = document.querySelector('header');
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 50) {
+
+    function updateHeader() {
+        if (window.pageYOffset > 50) {
             header.classList.add('scrolled');
         } else {
             header.classList.remove('scrolled');
         }
-    });
+    }
+
+    window.addEventListener('scroll', updateHeader);
+    // Initial check in case of refresh
+    updateHeader();
 });
